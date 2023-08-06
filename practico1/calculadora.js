c24ba029c7689ceaf411177d85de485995bb9395
mksh87 <keshishian.marcos@gmail.com>
2023-08-06 11:40:23 -0300

    practico1: primer versión de entrega

--- practico1/calculadora.js
+++ practico1/calculadora.js
@@ -2,26 +2,15 @@ let val1;
 let val2;
 let operacion;
 let resultado;
-let display="";
+let display = "";
 
-function calcular(){
-    const valorIngresado = document.getElementById('numero1').value;
-    // Puedes validar aquí si el valor ingresado es un número válido antes de almacenarlo
-    val1 = Number(valorIngresado);
-    console.log("Valor almacenado en val1:", val1);
-    
-    const valorIngresado2 = document.getElementById('numero2').value;
-    // Puedes validar aquí si el valor ingresado es un número válido antes de almacenarlo
-    val2 = Number(valorIngresado2);
-    console.log("Valor almacenado en val2:", val2);
+function calcular() {
+    val1 = Number(document.getElementById('numero1').value);
+    val2 = Number(document.getElementById('numero2').value);
+    operacion = document.getElementById('operacion').value;
 
-    const operacionElegida = document.getElementById('operacion');
-    operacion = operacionElegida.value;
-    console.log("Operacion elegida:", operacion);
-
-    let resultado =eval(val1+operacion+val2);
-    console.log("Resultado:", resultado);
+    let resultado = eval(val1 + operacion + val2);
 
     display = document.querySelector('.mostrarResultado');
-    display.textContent = "El resultado es: "+resultado;
+    display.textContent = "El resultado es: " + resultado;
 }
